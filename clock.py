3704c90d618cdbb0ce0b1a583280604890e3ff5b
Anirudh  Anup <anirudh012009@gmail.com>
2021-09-21 09:25:12 +0530

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,11 +1,4 @@
-#Substitute Clock For PyGame
-#Copyright (C) 2005  Adam Bark apb_4@users.sourceforge.net
-#Part of SpaceFlight2D
-#
-#SpaceFlight2D is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+
 
 from time import sleep, time
 class ABClock:
